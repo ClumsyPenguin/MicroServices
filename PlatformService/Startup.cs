5757ff46772f682dc2c752e4e98a90ab825c6915
ClumsyPenguin <seppe318@gmail.com>
2025-02-04 16:17:36 +0100

    Added migrations and updated the code

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -14,36 +14,35 @@ namespace PlatformService
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-        private IWebHostEnvironment _env { get; }
+        private IConfiguration Configuration { get; }
+        private IWebHostEnvironment Env { get; }
         
         public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
-            _env = env;
+            Env = env;
         }
         
         public void ConfigureServices(IServiceCollection services)
         {
-            if (_env.IsProduction())
-            {
-                services.AddDbContext<AppDbContext>(opt =>
-                    opt.UseSqlServer(Configuration.GetConnectionString("PlatfromsConn")));
-            }
-            else
-            {
-                Console.WriteLine("Using In Memory database");
-                services.AddDbContext<AppDbContext>(opt =>
-                    opt.UseInMemoryDatabase("InMem"));
-            }
-            services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
-            services.AddScoped<IPlatformRepo, PlatformRepo>();
-            services.AddControllers();
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
-            });
+             if (Env.IsProduction())
+                 services.AddDbContext<AppDbContext>(opt => 
+                     opt.UseSqlServer(Configuration.GetConnectionString("PlatfromsConn")));
+             else
+             {
+                 Console.WriteLine("Using In Memory database");
+                 services.AddDbContext<AppDbContext>(opt =>
+                     opt.UseInMemoryDatabase("InMem")); 
+             }
+             
+             services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>(); 
+             services.AddScoped<IPlatformRepo, PlatformRepo>(); 
+             services.AddControllers(); 
+             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+             services.AddSwaggerGen(c =>
+             { 
+                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
+             });
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -63,7 +62,7 @@ namespace PlatformService
                 endpoints.MapControllers();
             });
 
-            PrepDb.PrepPopulation(app);
+            PrepDb.PrepPopulation(app, env.IsProduction());
         }
     }
 }
